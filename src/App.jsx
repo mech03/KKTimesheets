dbff00824c554d2386529af6b0a202fa6e0a5295
Ogieltaziba Douglas <oxd184@student.bham.ac.uk>
2025-08-14 14:50:29 +0100

    timesheet view changed

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,10 @@ function App() {
       <div
         style={{
           display: "flex",
-          flexWrap: "wrap",
           gap: "20px",
-          padding: "20px",
+          padding: "10px 20px",
+          marginTop: "0",
+          minHeight: "calc(100vh - 60px)",
         }}
       >
         <TimesheetForm onSubmit={handleTimesheetSubmit} />
